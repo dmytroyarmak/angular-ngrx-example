91bd143cc8aef7e3a3dbc84976b6726119fd1d11
Dmytro Yarmak <dmytroy@backbase.com>
2017-05-05 20:46:21 +0300

    Change components prefix to dy

--- src/app/modules/core/components/root/root.component.ts
+++ src/app/modules/core/components/root/root.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'dy-root',
   templateUrl: './root.component.html',
   styleUrls: ['./root.component.css']
 })
